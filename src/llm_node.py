589e3269e0f38b6a9ae88d3651e488c4ef9b5b94
masataka46 <t.ohmasa@w-farmer.com>
2025-04-04 17:19:32 +0900

    llm code -> node

--- src/llm_node.py
+++ src/llm_node.py
@@ -1,12 +1,14 @@
-# from openai import OpenAI
-import os
-from langchain_openai import OpenAI
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Optional
 from dotenv import load_dotenv
-import base64
 import argparse
-import openai as openai_vanila
+import os
+import datetime
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+from state import OCRState
 
 def parser():
     parser = argparse.ArgumentParser(description='lllm_args')
@@ -17,188 +19,37 @@ def parser():
 
 def encode_image_to_base64(image_path:str) -> str:
     with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")    
+        return base64.b64encode(image_file.read()).decode("utf-8") 
 
-def main(args:argparse.Namespace) -> None:
-    # .envファイルの読み込み
+def ask_to_llm(state: OCRState) -> OCRState:
+    pass
+
+
+def main(args):
     load_dotenv()
 
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    langchain_tracing_v2 = os.getenv("LANGCHAIN_TRACING_V2")
-    langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
-    langchain_project = os.getenv("LANGCHAIN_PROJECT")
+    AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT = os.getenv("AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT")
+    AZURE_DOCUMENT_INTELLIGENCE_KEY = os.getenv("AZURE_DOCUMENT_INTELLIGENCE_KEY")
 
-    # 環境変数に設定
-    print("openai_api_key ", openai_api_key)
-    os.environ["OPENAI_API_KEY"] = openai_api_key
-    os.environ["LANGCHAIN_TRACING_V2"] = langchain_tracing_v2
-    os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
-    os.environ["LANGCHAIN_PROJECT"] = langchain_project
-
-    # model = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
-    model = ChatOpenAI(model="gpt-4-vision-preview", temperature=0)
-    # model = ChatOpenAI(model="gpt-4-turbo", temperature=0)
-    # model = ChatOpenAI(model="gpt-4-o", temperature=0)
-
-    image_path = "../images/flowchart-example{}.png".format(args.img_num)
-    image_base64 = encode_image_to_base64(image_path) # encode
-
-    prompt_with_arrow_163 = """以下のflow-chart図の読み取り結果を参考にして、下記の質問に答えてください。
-
-    ## flow-chart図の読み取り結果
-    1)type: terminator, text: Start, object_num: 1, 
-            before_object: none,
-            after_object: 'type: process, text: Sensor Starts Up' object_num: 2, 
-    2)type: process, text: Sensor Starts Up,  object_num: 2, 
-            before_object: 'type: terminator, text: Start', object_num: 1, 
-            after_object: 'type:process, text: Sensor Ready' object_num: 3, 
-    3)type: process, text: Sensor Ready, object_num: 3, 
-            before_object: 'type: process, text: Sensor Starts Up', object_num: 2, 
-            after_object: 'type:process, text: Sensor Detects for Gas' object_num: 4, 
-    4)type: process, text: Sensor Detects for Gas, object_num: 4, 
-            before_object: 'type:process, text: Sensor Ready, object_num: 3', 'type: process, text:System Updates, object_num: 15', 
-            after_object: 'type:data, text: Data Sent to Arduino object_num: 5'
-    5)type: data, text: Data Sent to Arduino, object_num: 5, 
-            before_object: 'type: process, text: Sensor Detects for Gas, object_num: 4', 
-            after_object: 'dicision, text: Gas?, object_num: 6, 
-    6)type: dicision, text: Gas?, object_num: 6, 
-            before_object: 'type:data, text: Data Sent to Arduino, object_num: 5',
-            after_object: Yes:'type:connection object_num: 7', no: 'type: data, text: Gas < 200, object_num: 8',
-    ...
-    15)type: process, text:System Updates, object_num: 15, 
-            before_object: 'type:connection, object_num: 14',
-            after_object: 'type: process, text: Sensor Detects for Gas, object_num: 4'
-
-    ## 質問
-    この画像でSystem Updateがなされた場合、次のprocessは何ですか？次の4つから1つ選んで答えてください。1)Sensor Starts Up, 2)Sensor Detects for Gas, 3)Display: 'Gas: SAFE' No Buzzer Green LED, 4) Display: 'Gas:WARNING!' Buzzer Yellow LED.
-    """
-
-    imgNum_prompt_dict = {
-                        #   '163': "この画像でGasだと判明した場合、次のprocessはどうなりますか？",
-                        #   '163': "この画像でSystem Updateがなされた場合、次のprocessは何ですか？",
-                        #   '163':  "この画像でSystem Updateがなされた場合、次のprocessは何ですか？次の4つから1つ選んで答えてください。1)Sensor Starts Up, 2)Sensor Detects for Gas, 3)Display: 'Gas: SAFE' No Buzzer Green LED, 4) Display: 'Gas:WARNING!' Buzzer Yellow LED.",
-                          '163':  prompt_with_arrow_163,
-
-                        #   '165': "この画像でPlayer's 2 Turnの次の処理は何ですか？",
-                        #   '165': "この画像でDid any player connect three in a rowでYesの場合、どうなりますか？",
-                          '165': "この画像でOne player will be X and the Other will be Oの次のprocessは何ですか？",
-                        #   '166': "この画像でDesignされたものがTestに通らない場合、どのようなprocessとなりますか？",
-                          '166': "この画像でPrototypeでない場合、次のprocessは何ですか？",
-                        #   '203': "この画像でRepair the coded charging schedule の次はどのようなprocessとなりますか？",
-                        #   '203': "この画像でHave all particle been processedでNoとなるとどのprocessとなりますか？",
-                          '203': "この画像でRepair the coded charging schedule の次はどのようなprocessとなりますか？次の4つの選択肢から選んでください。1)Calculate velocity vector, 2)Decode the charging schedule, 3)Go to next particle, 4) Move the particle",
-                          }
-
-    messages = [
-        SystemMessage(content="You are a helpful assistant that can analyze images."),
-        HumanMessage(
-            content=[
-                {"type": "text", "text": imgNum_prompt_dict['{}'.format(args.img_num)]}, # 163
-                # {"type": "image_url", "image_url": f"data:image/png;base64,{image_base64}"}
-                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{image_base64}"}}
-            ]
-        )
-    ]
-
-    # output = model.invoke("自己紹介してください")
-    output = model.invoke(messages)
-
-    print("output ", output)
-
-def not_use_langchain(args:argparse.Namespace) -> None:
-    # .envファイルの読み込み
-    load_dotenv()
-
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    os.environ["OPENAI_API_KEY"] = openai_api_key
-
-    image_path = "../images/flowchart-example{}.png".format(args.img_num)
-    image_base64 = encode_image_to_base64(image_path) # encode
-
-    prompt_with_arrow_163 = """以下のflow-chart図の読み取り結果を参考にして、下記の質問に答えてください。
-
-    ## flow-chart図の読み取り結果
-    1)type: terminator, text: Start, object_num: 1, 
-            before_object: none,
-            after_object: 'type: process, text: Sensor Starts Up' object_num: 2, 
-    2)type: process, text: Sensor Starts Up,  object_num: 2, 
-            before_object: 'type: terminator, text: Start', object_num: 1, 
-            after_object: 'type:process, text: Sensor Ready' object_num: 3, 
-    3)type: process, text: Sensor Ready, object_num: 3, 
-            before_object: 'type: process, text: Sensor Starts Up', object_num: 2, 
-            after_object: 'type:process, text: Sensor Detects for Gas' object_num: 4, 
-    4)type: process, text: Sensor Detects for Gas, object_num: 4, 
-            before_object: 'type:process, text: Sensor Ready, object_num: 3', 'type: process, text:System Updates, object_num: 15', 
-            after_object: 'type:data, text: Data Sent to Arduino object_num: 5'
-    5)type: data, text: Data Sent to Arduino, object_num: 5, 
-            before_object: 'type: process, text: Sensor Detects for Gas, object_num: 4', 
-            after_object: 'dicision, text: Gas?, object_num: 6, 
-    6)type: dicision, text: Gas?, object_num: 6, 
-            before_object: 'type:data, text: Data Sent to Arduino, object_num: 5',
-            after_object: Yes:'type:connection object_num: 7', no: 'type: data, text: Gas < 200, object_num: 8',
-    ...
-    15)type: process, text:System Updates, object_num: 15, 
-            before_object: 'type:connection, object_num: 14',
-            after_object: 'type: process, text: Sensor Detects for Gas, object_num: 4'
-
-    ## 質問
-    この画像でSystem Updateがなされた場合、次のprocessは何ですか？次の4つから1つ選んで答えてください。1)Sensor Starts Up, 2)Sensor Detects for Gas, 3)Display: 'Gas: SAFE' No Buzzer Green LED, 4) Display: 'Gas:WARNING!' Buzzer Yellow LED.
-    """
-
-    imgNum_prompt_dict = {
-                        #   '134':  "この画像でLogic & Data ErrorでYESの場合、次のprocessは何ですか？",
-                        #   '134':  "この画像でSystem Readyの次のprocessは何ですか？", # -> 「System Ready」の次のプロセスは「Enter Program」です。
-                        #   '134':  "この画像でProgram Codeの次のprocessは何ですか？", # -> Program Codeの次のプロセスは「Enter Program」です。
-                        #   '134':  "この画像でEnter Programの次のprocessは何ですか？", # -> この画像の「Enter Program」の次のプロセスは「Edit Source」です。
-                        #   '134':  "この画像でEdit Sourceの次のprocessは何ですか？", # -> この画像で「Edit Source」の次のプロセスは「Compile Source」です。
-                        #   '134':  "この画像でCompile Sourceの次のprocessは何ですか？", # -> 「Compile Source」の次のプロセスは「Syntax Error」です。
-                        #   '134':  "この画像でSyntax ErrorでYESの場合、次のprocessは何ですか？", # -> 画像では、"Syntax Error" が "YES" の場合、次のプロセスは "Edit Source" です。
-                        #   '134':  "この画像でSyntax ErrorでNOの場合、次のprocessは何ですか？", # -> このフローチャートで、Syntax ErrorがNOの場合、次のプロセスは「Link System Library」です。
-                        #   '134':  "この画像でSystem Libraryの次のprocessは何ですか？", # -> System Libraryの次のプロセスは「Link System Library」です。
-                        #   '134':  "この画像でLink System Libraryの次のprocessは何ですか？", # -> Link System Libraryの次のプロセスは「Execute System Code」です。
-                        #   '134':  "この画像でExecute System Codeyの次のprocessは何ですか？", # -> 画像のフローチャートによれば、「Execute System Code」の次のプロセスは「Logic & Data Error」の判定です。ここで「YES」の場合は「Edit Source」に戻り、「NO」の場合は「Correct Code」に進みます。
-                          '134':  "この画像でLogic & Data ErrorでNOの場合、次のprocessは何ですか？", # -> このフローチャートでは、「Logic & Data Error」が「NO」の場合、次のプロセスは「Correct Code」で、その後「STOP」となります
-
-                          '151':  "この画像でPick up items from current inventoryの次のprocessは何ですか？",
-                        #   '163': "この画像でGasだと判明した場合、次のprocessはどうなりますか？",
-                          '163': "この画像でSystem Updateがなされた場合、次のprocessは何ですか？",
-                        #   '163':  "この画像でSystem Updateがなされた場合、次のprocessは何ですか？次の4つから1つ選んで答えてください。1)Sensor Starts Up, 2)Sensor Detects for Gas, 3)Display: 'Gas: SAFE' No Buzzer Green LED, 4) Display: 'Gas:WARNING!' Buzzer Yellow LED.",
-                        #   '163':  prompt_with_arrow_163,
-
-                        #   '165': "この画像でPlayer's 2 Turnの次の処理は何ですか？",
-                        #   '165': "この画像でDid any player connect three in a rowでYesの場合、どうなりますか？",
-                          '165': "この画像でOne player will be X and the Other will be Oの次のprocessは何ですか？",
-                        #   '166': "この画像でDesignされたものがTestに通らない場合、どのようなprocessとなりますか？",
-                          '166': "この画像でPrototypeでない場合、次のprocessは何ですか？",
-                          '187': "この画像で The patient goes back home to check in later の次のprocessは何ですか？",
-
-                        #   '203': "この画像でRepair the coded charging schedule の次はどのようなprocessとなりますか？",
-                          '203': "この画像でHave all particle been processedでNoとなるとどのprocessとなりますか？",
-                        #   '203': "この画像でRepair the coded charging schedule の次はどのようなprocessとなりますか？次の4つの選択肢から選んでください。1)Calculate velocity vector, 2)Decode the charging schedule, 3)Go to next particle, 4) Move the particle",
-                          }
-
-    message1=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": imgNum_prompt_dict['{}'.format(args.img_num)]},
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{image_base64}"
-                    },
-                },
-            ],
-        },
-    ]
-
-    response = openai_vanila.chat.completions.create(
-        model="gpt-4o",
-        messages=message1,
-        max_tokens=300,
+    documentAnalysisClient1 = DocumentAnalysisClient(
+        endpoint=AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT,
+        credential=AzureKeyCredential(AZURE_DOCUMENT_INTELLIGENCE_KEY)
     )
 
-    print(response.choices[0].message.content)
+    builder = StateGraph(OCRState)
+    builder.add_node("RunOCR", run_azure_ocr)
+    builder.add_node("ShowResult", print_result)
+
+    builder.set_entry_point("RunOCR")
+    builder.add_edge("RunOCR", "ShowResult")
+    builder.add_edge("ShowResult", END)
+
+    graph = builder.compile()
+
+    # run langgraph
+    input_state = {"image_path": args.img_path, "extracted_text": None, "result": None, "document_analysis_client": documentAnalysisClient1,
+    "out_dir": args.output_dir}
+    final_state = graph.invoke(input_state)
 
 if __name__ == '__main__':
     """
